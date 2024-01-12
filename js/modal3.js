a03bc3cef1d4c4d87414c9112e9fbaac0edd802f
web1004 <gangsugi@gmail.com>
2024-01-12 12:32:39 +0900

    Modify-24.1.12

--- js/modal3.js
+++ js/modal3.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   //각 목록을 클릭했을때.....
   $(".menu").click(function(){ 
     $(this).next().show();
-    $("html").css({"overflow-y":"hidden"}); 
-    return false;
+    $("html").css({"overflow-y":"hidden"});
+    //return false;  a기능차단으로 넣지만 지금은 연결된 상태가 아니므로 넣지 않아도 됨 
   });
 
   //close버튼과 검정배경영역을 클릭할때.....
--- js/modal4.js
+++ js/modal4.js
@@ -6,36 +6,32 @@ $(document).ready(function(){
     g_pop=$(this).index();
 
     $(".g_page span:nth-child(1)").text(g_pop+1);  //오른쪽 상단 페이지번호
-    $("html").css({overflowY:"hidden"}); //기존 html 스크롤 숨기기
+    $("html").css({"overflow-y":"hidden"}); //기존 html 스크롤 숨기기
     $(".pop>li").eq(g_pop).show();  //각 목록의 내용
     $("#popup").stop().fadeIn(); //검정배경
-  
+
   });
 
   //이전다음버튼....
   $(".left_btn").click(function(){
-
     if(g_pop>0){
-      $(".pop>li").eq(g_pop).stop().fadeOut(); //기존거 사라짐
+      $(".pop>li").eq(g_pop).stop().fadeOut(); 
       g_pop--;
       $(".g_page span:nth-child(1)").text(g_pop+1);
 			$(".pop>li").eq(g_pop).stop().fadeIn();
     };
-
   });
 
   $(".right_btn").click(function(){
-
     if(g_pop<14){
-      $(".pop>li").eq(g_pop).stop().fadeOut(); //기존거 사라짐
+      $(".pop>li").eq(g_pop).stop().fadeOut(); 
       g_pop++;
       $(".g_page span:nth-child(1)").text(g_pop+1);
 			$(".pop>li").eq(g_pop).stop().fadeIn();
     };
-
   });
 
-  //닫기버튼.....
+   //닫기버튼.....
   $(".btn_close").click(function(){
     $("html").css({"overflow-y":"scroll"}); 
     $("#popup").stop().fadeOut();
