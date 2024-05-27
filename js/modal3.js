f3323df7e6eefc4852fde0be73125c785ce64d07
web1004 <gangsugi@gmail.com>
2024-05-27 16:48:08 +0900

    수정-240527

--- js/modal3.js
+++ js/modal3.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	
   //각 목록을 클릭했을때.....
   $(".menu").click(function(){ 
-    $(this).next().show();
+    $(this).next().fadeIn();
     $("html").css({"overflow-y":"hidden"});
     //return false;  a기능차단으로 넣지만 지금은 연결된 상태가 아니므로 넣지 않아도 됨 
   });
 
   //close버튼과 검정배경영역을 클릭할때.....
-  $(".close, .pop").click(function(){
-    $(".pop").hide(); 
+  $(".close, .modal").click(function(){
+    $(".modal").fadeOut(); 
     $("html").css({"overflow-y":"scroll"}); 
   });
 
--- js/modal4.js
+++ js/modal4.js
@@ -7,35 +7,35 @@ $(document).ready(function(){
 
     $(".g_page span:nth-child(1)").text(g_pop+1);  //오른쪽 상단 페이지번호
     $("html").css({"overflow-y":"hidden"}); //기존 html 스크롤 숨기기
-    $(".pop>li").eq(g_pop).show();  //각 목록의 내용
-    $("#popup").stop().fadeIn(); //검정배경
+    $(".modal-content>li").eq(g_pop).fadeIn();  //각 목록의 내용
+    $(".modal").stop().fadeIn(); //검정배경
 
   });
 
   //이전다음버튼....
-  $(".left_btn").click(function(){
+  $(".pre").click(function(){
     if(g_pop>0){
-      $(".pop>li").eq(g_pop).stop().fadeOut(); 
+      $(".modal-content>li").eq(g_pop).stop().fadeOut(); 
       g_pop--;
       $(".g_page span:nth-child(1)").text(g_pop+1);
-			$(".pop>li").eq(g_pop).stop().fadeIn();
+			$(".modal-content>li").eq(g_pop).stop().fadeIn();
     };
   });
 
-  $(".right_btn").click(function(){
+  $(".next").click(function(){
     if(g_pop<14){
-      $(".pop>li").eq(g_pop).stop().fadeOut(); 
+      $(".modal-content>li").eq(g_pop).stop().fadeOut(); 
       g_pop++;
       $(".g_page span:nth-child(1)").text(g_pop+1);
-			$(".pop>li").eq(g_pop).stop().fadeIn();
+			$(".modal-content>li").eq(g_pop).stop().show();
     };
   });
 
    //닫기버튼.....
-  $(".btn_close").click(function(){
+  $(".close").click(function(){
     $("html").css({"overflow-y":"scroll"}); 
-    $("#popup").stop().fadeOut();
-		$(".pop>li").stop().hide();
+    $(".modal").stop().fadeOut();
+		$(".modal-content>li").stop().hide();
   });
 
 });
